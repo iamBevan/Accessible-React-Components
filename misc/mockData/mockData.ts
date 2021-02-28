9caef4b8076811c323a712bba9e0f45184364bb7
Kevin Bevan <kevin.bevan@cdsm.co.uk>
2021-02-28 04:36:03 +0000

    setup some mockdata for dropdown

---
+++ misc/mockData/mockData.ts
@@ -0,0 +1,18 @@
+import { Item } from "./interfaces"
+
+export const items: Item[] = [
+	{ name: "Raptor", id: "raptor" },
+	{ name: "Mouse", id: "mouse" },
+	{ name: "Ox", id: "ox" },
+	{ name: "Elk", id: "elk" },
+	{ name: "Monkey", id: "monkey" },
+	{ name: "Owl", id: "owl" },
+	{ name: "Crab", id: "crab" },
+	{ name: "Otter", id: "otter" },
+	{ name: "Dingo", id: "dingo" },
+	{ name: "Fruit Bat", id: "fruitbat" },
+	{ name: "Ostritch", id: "ostritch" },
+	{ name: "Eel", id: "eel" },
+	{ name: "Kestral", id: "kestral" },
+	{ name: "Spider", id: "spider" },
+]
