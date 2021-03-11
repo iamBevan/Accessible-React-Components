b1be7f7039e685fd07aa8426549b4540d9981cdc
Kevin Bevan <iambevan@gmail.com>
2021-03-11 03:15:05 +0000

    remove head

--- pages/components.tsx
+++ pages/components.tsx
@@ -1,13 +1,9 @@
-import Head from "next/head"
 import React from "react"
 import styles from "../styles/pages/components.module.scss"
 
 export default function Components(): JSX.Element {
 	return (
 		<div className={styles["container"]}>
-			<Head>
-				<title>Components</title>
-			</Head>
 			<h1>Components</h1>
 		</div>
 	)
