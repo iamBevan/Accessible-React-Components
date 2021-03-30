52747e891348dbfff734646bd6fcff1316e6dd96
Kevin Bevan <iambevan@gmail.com>
2021-03-30 01:36:34 +0100

    update button props

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -2,11 +2,40 @@ import React from "react"
 import styles from "./button.module.scss"
 
 interface ButtonProps {
+	/**
+	 * Text displayed on the button
+	 */
 	text: string
+	/**
+	 * When an action associated with the button is unavailable, the button
+	 * has aria-disabled set to true - button can still be focused (see disabled prop).
+	 */
+	ariaDisabled?: boolean
+	/**
+	 * When set to true, it specifies that the button should be disabled - button can not receive focus.
+	 */
+	disabled?: boolean
+	/**
+	 * The button has an accessible label. By default, the accessible name
+	 * is computed from any text content inside the button element. However,
+	 * it can also be provided with an aria-label.
+	 */
+	ariaLabel?: string
 }
 
-const Button: React.FC<ButtonProps> = ({ text }) => {
-	return <button className={styles["button"]}>{text}</button>
+const Button: React.FC<ButtonProps> = ({ text, disabled, ariaLabel }) => {
+	return (
+		<>
+			<button
+				className={styles["button"]}
+				aria-label={ariaLabel ?? undefined}
+				aria-disabled={disabled ?? false}
+				disabled={disabled ?? false}
+			>
+				{text}
+			</button>
+		</>
+	)
 }
 
 export { Button }
