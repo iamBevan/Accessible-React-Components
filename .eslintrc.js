d7071293b63c3adba2e2e7af41c9fbd00f117a0a
Kevin Bevan <iambevan@gmail.com>
2021-03-31 02:04:24 +0100

    remove commented rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -68,10 +68,6 @@ module.exports = {
 		"@typescript-eslint/no-unsafe-assignment": "off",
 		"@typescript-eslint/no-unsafe-member-access": "off",
 		"@typescript-eslint/no-unsafe-call": "off",
-		// "@typescript-eslint/await-thenable": "off",
-		// "@typescript-eslint/no-floating-promises": "off",
-		// "@typescript-eslint/no-implied-eval": "off",
-		// "@typescript-eslint/no-misused-promises": "off",
 	},
 	settings: {
 		react: {
