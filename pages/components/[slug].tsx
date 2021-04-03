e963d49d6cf868f964153ff2152323cd428c1175
Kevin Bevan <iambevan@gmail.com>
2021-04-03 06:08:39 +0100

    refactor and clean up types

---
+++ pages/components/[slug].tsx
@@ -0,0 +1,54 @@
+import { GetStaticPathsResult, GetStaticPropsResult } from "next"
+import dynamic from "next/dynamic"
+import { getPageBySlug, getAllPages } from "../../lib/pages"
+
+interface PageProps {
+	slug: string
+}
+
+interface StaticProps {
+	params: { [key: string]: string }
+	slug: string
+}
+
+interface StaticPropsResult {
+	slug: string
+	content: string
+	meta: { [key: string]: string }
+}
+
+export default function Page({ slug }: PageProps): JSX.Element {
+	const MDXContent = dynamic(() => import(`../../docs/${slug}.mdx`))
+
+	return <MDXContent />
+}
+
+export function getStaticProps({
+	params,
+}: StaticProps): GetStaticPropsResult<StaticPropsResult> {
+	const page = getPageBySlug(params.slug)
+
+	return {
+		props: {
+			slug: page.slug,
+			content: page.content,
+			// eslint-disable-next-line @typescript-eslint/no-var-requires
+			meta: require(`../../docs/${page.slug}.mdx`).meta,
+		},
+	}
+}
+
+export function getStaticPaths(): GetStaticPathsResult {
+	const pages = getAllPages()
+
+	return {
+		paths: pages.map(page => {
+			return {
+				params: {
+					slug: page.slug,
+				},
+			}
+		}),
+		fallback: true,
+	}
+}
