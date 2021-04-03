6afc4e8576d69fa36befab4b094282f9014676eb
Kevin Bevan <iambevan@gmail.com>
2021-04-03 06:09:15 +0100

    wip - add MDXProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,25 @@
 import { Fragment } from "react"
 import { Layout } from "../components/layout/layout"
 import { AppProps } from "next/app"
+import { MDXProvider } from "@mdx-js/react"
 import "../styles/globals.scss"
+
+const mdComponents = {
+	h1: (props: { children: string }) => (
+		<>
+			<h1 style={{ color: "tomato" }} {...props} />
+		</>
+	),
+}
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
 	const Container = pageProps.layout === "components" ? Layout : Fragment
 
 	return (
 		<Container>
-			<Component {...pageProps} />
+			<MDXProvider components={mdComponents}>
+				<Component {...pageProps} />
+			</MDXProvider>
 		</Container>
 	)
 }
