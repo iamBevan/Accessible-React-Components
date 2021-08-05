b9cf8b5a76d346e955ff0b1643f60b03196c7de3
Kevin Bevan <iambevan@gmail.com>
2021-08-05 01:49:04 +0100

    add progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,20 @@ import { AppProps } from "next/app"
 import { MDXProvider } from "@mdx-js/react"
 import "../styles/globals.scss"
 import MDXComponents from "../helpers/MDXComponents"
+import NextNProgress from "nextjs-progressbar"
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
 	const Container = pageProps.layout === "components" ? Layout : Fragment
 
 	return (
 		<Container>
+			<NextNProgress
+				color='var(--focus)'
+				startPosition={0.3}
+				stopDelayMs={0}
+				height={2}
+				showOnShallow={false}
+			/>
 			<MDXProvider components={MDXComponents}>
 				<Component {...pageProps} />
 			</MDXProvider>
