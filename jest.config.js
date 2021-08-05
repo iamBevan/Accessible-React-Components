55526203f9602ff9487fd3b4c172fbeabf545522
Kevin Bevan <iambevan@gmail.com>
2021-08-05 02:34:37 +0100

    amend github workflow

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-    testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-    moduleDirectories: ['node_modules', 'src'],
-    moduleNameMapper: {
-        '\\.(scss|sass|css)$': 'identity-obj-proxy',
-      },
-  };
+	setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+	testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+	moduleDirectories: ["node_modules", "src/**/**"],
+	moduleNameMapper: {
+		"\\.(scss|sass|css)$": "identity-obj-proxy",
+	},
+}
