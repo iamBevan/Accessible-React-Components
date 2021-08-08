f4110d6eef0ed3e1d8112cecc090cfb3abf3d459
Kevin Bevan <iambevan@gmail.com>
2021-08-08 03:36:22 +0100

    add onClick to Checkbox

--- components/checkbox/checkbox.tsx
+++ components/checkbox/checkbox.tsx
@@ -1,13 +1,19 @@
 import React from "react"
 
-interface CheckboxProps {
+export interface CheckboxProps {
 	label: string
 	checked: boolean
+	setChecked: () => void
 }
 
-const Checkbox: React.FC<CheckboxProps> = ({ label, checked }) => {
+const Checkbox: React.FC<CheckboxProps> = ({ label, checked, setChecked }) => {
 	return (
-		<div role='checkbox' aria-checked={checked} tabIndex={0}>
+		<div
+			role='checkbox'
+			aria-checked={checked}
+			tabIndex={0}
+			onClick={setChecked}
+		>
 			{label}
 		</div>
 	)
