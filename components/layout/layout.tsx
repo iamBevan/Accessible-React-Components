bb48bd567dbb8c7ed2ea4f4d2dbb8f27bfdd59ca
Kevin Bevan <iambevan@gmail.com>
2021-08-30 03:17:32 +0100

    🔥 remove buggy sidebar

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,89 +1,62 @@
 import { ReactNode } from "react"
-import { useMedia } from "react-use"
 import Link from "next/link"
 import styles from "./layout.module.scss"
-import { Sidebar } from "../sidebar/sidebar"
 
 interface LayoutProps {
 	children: ReactNode
 }
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
-	const isWide = useMedia("(min-width: 800px)")
-
 	return (
-		<>
-			{isWide ? (
-				<div className={styles["container"]}>
-					<div className={styles["sidebar"]}>
-						<div className={styles["heading"]}>
-							<h1>
-								<Link href='/'>Flamingo</Link>
-							</h1>
-							<span role='img' aria-label='flamingo'>
-								🦩
-							</span>
-						</div>
-						<nav className={styles["nav"]}>
-							<ul>
-								<li>
-									<Link href='/components/button'>
-										Button
-									</Link>
-								</li>
-								<li>
-									<Link href='/components/checkbox'>
-										Checkbox
-									</Link>
-								</li>
-								<li>
-									<Link href='/components/checkbox'>
-										Switch
-									</Link>
-								</li>
-								<li>
-									<Link href='/components/slider'>
-										Slider
-									</Link>
-								</li>
-								<li>
-									<Link href='/components/dropdown'>
-										Dropdown
-									</Link>
-								</li>
-								<li>
-									<Link href='/components/accordion'>
-										Accordion
-									</Link>
-								</li>
-								<li>
-									<Link href='/components/modal'>Modal</Link>
-								</li>
-								<li>
-									<Link href='/components/tooltip'>
-										Tooltip
-									</Link>
-								</li>
-								<li>
-									<Link href='/components/tabs'>Tabs</Link>
-								</li>
-								<li>
-									<Link href='/components/datetimepicker'>
-										Date/Time Picker
-									</Link>
-								</li>
-							</ul>
-						</nav>
-					</div>
-					<div className={styles["content"]}>{children}</div>
+		<div className={styles["container"]}>
+			<div className={styles["sidebar"]}>
+				<div className={styles["heading"]}>
+					<h1>
+						<Link href='/'>Flamingo</Link>
+					</h1>
+					<span role='img' aria-label='flamingo'>
+						🦩
+					</span>
 				</div>
-			) : (
-				<div className={styles["container"]}>
-					<Sidebar />
-					<div className={styles["content"]}>{children}</div>
-				</div>
-			)}
-		</>
+				<nav className={styles["nav"]}>
+					<ul>
+						<li>
+							<Link href='/components/button'>Button</Link>
+						</li>
+						<li>
+							<Link href='/components/checkbox'>Checkbox</Link>
+						</li>
+						<li>
+							<Link href='/components/checkbox'>Switch</Link>
+						</li>
+						<li>
+							<Link href='/components/slider'>Slider</Link>
+						</li>
+						<li>
+							<Link href='/components/dropdown'>Dropdown</Link>
+						</li>
+						<li>
+							<Link href='/components/accordion'>Accordion</Link>
+						</li>
+						<li>
+							<Link href='/components/modal'>Modal</Link>
+						</li>
+						<li>
+							<Link href='/components/tooltip'>Tooltip</Link>
+						</li>
+						<li>
+							<Link href='/components/tabs'>Tabs</Link>
+						</li>
+						<li>
+							<Link href='/components/datetimepicker'>
+								Date/Time Picker
+							</Link>
+						</li>
+					</ul>
+				</nav>
+			</div>
+			<div className={styles["content"]}>{children}</div>
+		</div>
 	)
 }
 
