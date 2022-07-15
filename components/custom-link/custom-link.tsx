66c90759db69c2758b271a1f359676cd0ee26fc3
Kevin Bevan <iambevan@gmail.com>
2022-07-15 01:05:01 +0100

    update type

--- components/custom-link/custom-link.tsx
+++ components/custom-link/custom-link.tsx
@@ -1,7 +1,11 @@
 import Link, { LinkProps } from "next/link"
+import { ReactNode } from "react"
 import styles from "./custom-link.module.scss"
 
-const CustomLink: React.FC<LinkProps> = ({ href, children }) => {
+const CustomLink = ({
+	href,
+	children,
+}: LinkProps & { children: ReactNode }): JSX.Element => {
 	return (
 		<Link href={href}>
 			<a className={styles["link"]}>{children}</a>
